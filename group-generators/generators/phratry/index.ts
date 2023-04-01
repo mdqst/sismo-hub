0dc41d238d3b6633cdd5fbbcb4136cfa6b2053fe
Dmitry <98899785+mdqst@users.noreply.github.com>
2023-04-01 13:58:12 +0300

    Update index.ts

--- group-generators/generators/phratry/index.ts
+++ group-generators/generators/phratry/index.ts
@@ -17,7 +17,14 @@ const generator: GroupGenerator = {
     
     const jsonListData0 = {
       "0xB2fe5c3c7231B1117Da36e7B29731693fbBc6F7A": "1",
+      "0x5a869e3de0E797E4ff8e1b3436610a66eD09Bc93": "1",
+      "0x7eB6dBAad0fF7FC111abD7553457CFdFf1dDE10C": "1",
     };
+
+const dataUnion = dataOperators.Union([
+      zkDeveloperContributorData0,
+      jsonListData1,
+    ]);
 
     return [
       {
@@ -26,6 +33,7 @@ const generator: GroupGenerator = {
         description: "Phratry DAO Community Mambers Badge",
         specs: "",
         data: jsonListData0,
+        data: dataUnion,
         valueType: ValueType.Score,
         tags: [Tags.Factory],
       },
